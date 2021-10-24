db5c6d5c351735f55d148f709b656bc9b92aa5b8
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-10-24 13:01:45 -0300

    Implementa consulta por cpf ou email

--- src/main/java/com/rasmoo/api/rasfood/controller/ClienteController.java
+++ src/main/java/com/rasmoo/api/rasfood/controller/ClienteController.java
@@ -1,17 +1,15 @@
 package com.rasmoo.api.rasfood.controller;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rasmoo.api.rasfood.entity.Cliente;
 import com.rasmoo.api.rasfood.entity.ClienteId;
 import com.rasmoo.api.rasfood.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,18 +20,28 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping
-    private ResponseEntity<List<Cliente>> consultarTodos(){
+    public ResponseEntity<List<Cliente>> consultarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.findAll());
     }
 
     @GetMapping("/{email}/{cpf}")
-    private ResponseEntity<Cliente> consultarPorEmailCpf(@PathVariable("email") final String email, @PathVariable("cpf") final String cpf){
-        ClienteId clienteId = new ClienteId(email,cpf);
-        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-        if(!cliente.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    public ResponseEntity<Cliente> consultarPorEmailCpf(@PathVariable("email") final String email, @PathVariable("cpf") final String cpf){
+        return clienteRepository.findById(new ClienteId(email,cpf))
+                .map(value -> ResponseEntity.status(HttpStatus.OK).body(value))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable("id") final String id, @RequestBody  final Cliente cliente) throws JsonMappingException {
+        Optional<Cliente> clienteEncontrado = this.clienteRepository.findByEmailOrCpf(id);
+        if (clienteEncontrado.isPresent()){
+            objectMapper.updateValue(clienteEncontrado.get(),cliente);
+            return ResponseEntity.status(HttpStatus.OK).body(clienteEncontrado.get());
         }
-        return ResponseEntity.status(HttpStatus.OK).body(cliente.get());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
