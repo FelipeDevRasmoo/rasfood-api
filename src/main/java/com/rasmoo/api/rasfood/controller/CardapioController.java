6f4bdd14f816b2ded625897ef7dafff0deaade7b
Felipe Alves <felipe.alves.stefanini@elo.com.br>
2021-12-28 06:20:08 -0300

    Cria cardapio e carga de dados

--- src/main/java/com/rasmoo/api/rasfood/controller/CardapioController.java
+++ src/main/java/com/rasmoo/api/rasfood/controller/CardapioController.java
@@ -1,15 +1,63 @@
 package com.rasmoo.api.rasfood.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rasmoo.api.rasfood.entity.Cardapio;
+import com.rasmoo.api.rasfood.repository.CardapioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping(value = "/cardapio")
 @RestController
 public class CardapioController {
 
-    @GetMapping("/teste")
-    private String teste(){
-        return "Estou Viva!";
+    @Autowired
+    private CardapioRepository cardapioRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping
+    public ResponseEntity<List<Cardapio>> consultarTodos() {
+        return ResponseEntity.status(HttpStatus.OK).body(cardapioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cardapio> consultarPorId(@PathVariable("id") final Integer id) {
+        Optional<Cardapio> cardapioEncontrado = cardapioRepository.findById(id);
+        if (cardapioEncontrado.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(cardapioEncontrado.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirPorId(@PathVariable("id") final Integer id) {
+        Optional<Cardapio> cardapioEncontrado = cardapioRepository.findById(id);
+        if (cardapioEncontrado.isPresent()) {
+            cardapioRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cardapio> criar(@RequestBody final Cardapio cardapio) throws JsonMappingException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.cardapioRepository.save(cardapio));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Cardapio> atualizar(@PathVariable("id") final Integer id, @RequestBody final Cardapio cardapio) throws JsonMappingException {
+        Optional<Cardapio> cardapioEncontrado = this.cardapioRepository.findById(id);
+        if (cardapioEncontrado.isPresent()) {
+            objectMapper.updateValue(cardapioEncontrado.get(), cardapio);
+            return ResponseEntity.status(HttpStatus.OK).body(this.cardapioRepository.save(cardapioEncontrado.get()));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
