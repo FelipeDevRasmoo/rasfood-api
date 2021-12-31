eeee656fb69a357fc5bacd2eb7895801af323e13
Felipe Alves <felipe.alves.stefanini@elo.com.br>
2021-12-30 22:45:54 -0300

    Cria query modify

--- src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
+++ src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
@@ -4,9 +4,11 @@ import com.rasmoo.api.rasfood.dto.CardapioDto;
 import com.rasmoo.api.rasfood.entity.Cardapio;
 import com.rasmoo.api.rasfood.repository.projection.CardapioProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -25,4 +27,13 @@ public interface CardapioRepository extends JpaRepository<Cardapio,Integer> {
             "    INNER JOIN categorias cat on c.categoria_id = cat.id" +
             "    WHERE c.categoria_id = ?1 AND c.disponivel = true",nativeQuery = true)
     List<CardapioProjection> findAllByCategoria(final Integer categoria);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Cardapio c SET c.disponivel = " +
+            "CASE c.disponivel " +
+            "WHEN true THEN false " +
+            "ELSE true END " +
+            "WHERE c.id = :id")
+    Integer updateDisponibilidade(final Integer id);
 }
